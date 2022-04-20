7f537cfcc4c68ced102a709bd2315508d3b662e1
andreaminolfi <andrea@minolfi.it>
2022-04-20 15:29:26 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { LoginComponent } from './pages/login.component';
 import { CatalogComponent } from './pages/catalog.component';
 import { ContactsComponent } from './pages/contacts.component';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClient,
     RouterModule.forRoot([
       { path: 'login', component: LoginComponent },
       { path: 'catalog', component: CatalogComponent },
